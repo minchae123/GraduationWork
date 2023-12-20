0f0cd6e8be7103b4928922e790e27f99c46dbe65
Woo Hyeok <pwh060720@gmail.com>
2023-12-20 18:53:36 +0900

    플레이어 애니메이션 및 청소기 기능 부분 변경

--- WouldYouClean/Assets/01_Scripts/WooHyeok/Player/Cleaner/Cleaner.cs
+++ WouldYouClean/Assets/01_Scripts/WooHyeok/Player/Cleaner/Cleaner.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class Cleaner : MonoBehaviour
 {
-    [SerializeField] private Transform _gatherPos;
-
     [Header("참조")]
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Transform _gatherPos;
     [SerializeField] private PlayerDirection _direction;
     [SerializeField] private BoxCollider2D _boxCol;
 
     [Header("시간")]
+    [SerializeField] private float _speed;
     [SerializeField] private float _minSpeed;
     [SerializeField] private float _maxSpeed;
 
+    private Sequence _cleaningSequence;
+    private bool _isMouseClick;
 
     private void Update()
     {
         ColDir();
+        CleanAnim();
+        CheckMouseRelease();
     }
 
     private void ColDir()
@@ -31,16 +37,35 @@ public class Cleaner : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (!collision.GetComponent<DivideObj>()) return;
+        if (!collision.GetComponent<DivideObj>() || !_isMouseClick) return;
 
         float dis = Vector2.Distance(collision.transform.position, _gatherPos.position);
         float _dirTime = Mathf.Lerp(_minSpeed, _maxSpeed, Mathf.InverseLerp(0f, 10f, dis));
 
-        Vector2 dir = Vector2.MoveTowards(collision.transform.position, _gatherPos.position, _dirTime * Time.deltaTime);
-
-        collision.transform.position = dir;
+        collision.transform.DOMove(_gatherPos.position, _dirTime / _speed / 1.5f);
 
         float targetScale = Mathf.Lerp(1f, 0f, Mathf.InverseLerp(0f, 20f, dis));
-        collision.transform.DOScale(0, targetScale).OnComplete(()=>Destroy(collision.gameObject));
+
+        DotScale(collision.gameObject, targetScale);
+    }
+
+    private void CleanAnim()
+    {
+        _isMouseClick = Input.GetMouseButton(0);
+        _animator.SetBool("clean", _isMouseClick);
+    }
+
+    private void DotScale(GameObject obj, float targetScale)
+    {
+        _cleaningSequence = DOTween.Sequence();
+
+        _cleaningSequence.Append(obj.transform.DOScale(0, targetScale / 1.5f));
+        _cleaningSequence.OnComplete(() => Destroy(obj));
+    }
+
+    private void CheckMouseRelease()
+    {
+        if (Input.GetMouseButtonUp(0) && _cleaningSequence.IsActive())
+            _cleaningSequence.Kill();
     }
 }
--- WouldYouClean/Assets/01_Scripts/WooHyeok/Player/PlayerMain.cs
+++ WouldYouClean/Assets/01_Scripts/WooHyeok/Player/PlayerMain.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerMain : MonoBehaviour
 {
     protected Rigidbody2D _rb;
+    protected Animator _animator;
     protected SpriteRenderer _spriteRenderer;
     protected static bool _isKeyDown = false;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = transform.Find("Visual").GetComponent<Animator>();
         _spriteRenderer = transform.Find("Visual").GetComponent<SpriteRenderer>();
     }
 }
