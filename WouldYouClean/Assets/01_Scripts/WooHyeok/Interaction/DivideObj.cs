444f91add77fd4d696215a755474561edaa1c633
Woo Hyeok <pwh060720@gmail.com>
2023-12-27 00:03:35 +0900

    합친거

--- WouldYouClean/Assets/01_Scripts/WooHyeok/Interaction/DivideObj.cs
+++ WouldYouClean/Assets/01_Scripts/WooHyeok/Interaction/DivideObj.cs
@@ -6,11 +6,38 @@ using UnityEngine;
 public class DivideObj : MonoBehaviour
 {
     public ObjectType type;
+    public ObjectType Item => type;
 
+    private SpriteRenderer spriterenderer;
+
+
+    private void Awake()
+    {
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        spriterenderer.sprite = type.itemIcon;
+    }
 
     public void PickUpItem() // 추가
     {
-        Inventory.Instance.AddItem(type, true);
-        Destroy(gameObject);
+        if (Inventory.Instance.CheckInventoryIdx(type)) // 인벤토리 자리 남아있을 때에만
+        {
+            Inventory.Instance.AddItem(type, false); // 추가하고
+
+            Destroy(gameObject); // 삭제하고
+        }
     }
+
+    public void SetItemData(ObjectType data)
+    {
+        type = data;
+    }
+
+    public void SetPosition(Vector2 pos)
+    {
+        transform.position = pos;
+    }
+
 }
