c8186f902c5b1c40e3e28c870c4ae9b5d0447abd
Woo Hyeok <pwh060720@gmail.com>
2024-03-09 23:03:00 +0900

    .

--- WouldYouClean/Assets/01_Scripts/WooHyeok/Drawing/DrawPatten.cs
+++ WouldYouClean/Assets/01_Scripts/WooHyeok/Drawing/DrawPatten.cs
@@ -5,31 +5,87 @@ using UnityEngine;
 public class DrawPatten : MonoBehaviour
 {
     [SerializeField] private Camera cam;
+    [SerializeField] private RectTransform[] spots;
+
+    private Vector3 mousePosition;
     private LineRenderer line;
     private int count;
+
+    private float minDis = 100f;
+    private float dis;
 
     // Start is called before the first frame update
     void Start()
     {
         line = GetComponent<LineRenderer>();
 
+        mousePosition = Input.mousePosition;
         count = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    line.SetPosition(0, WorldToLocal(Input.mousePosition));
+        //    print(WorldToLocal(spots[0].position));
+        //}
+        //else if(Input.GetMouseButton(0))
+        //{
+        //    print("ss");
+        //    line.SetPosition(count, WorldToLocal(Input.mousePosition));
+        //}
+        //if(Input.GetMouseButtonUp(0))
+        //{
+        //    count++;
+        //}
+        CrossSpot();
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 worldPosition = ConvertUIToWorldCoordinates(mousePosition);
+        //    Debug.Log("UI Element World Position: " + worldPosition);
+        //}
+    }
+
+    private void CrossSpot()
+    {
+        Distance();
+
         if (Input.GetMouseButtonDown(0))
         {
-            line.SetPosition(0, WorldToLocal(Input.mousePosition));
+            if (minDis <= 1)
+                line.SetPosition(0, WorldToLocal(Input.mousePosition));
+            else
+                return;
         }
-        else if(Input.GetMouseButton(0))
+        else if (Input.GetMouseButton(0))
         {
-            line.SetPosition(count, WorldToLocal(Input.mousePosition));
+            Distance();
+
+            if (minDis <= 1)
+            {
+                Debug.Log(minDis);
+                line.SetPosition(count, WorldToLocal(Input.mousePosition));
+                count++;
+            }
         }
-        if(Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0))
         {
-            count++;
+            line.positionCount = 2;
+        }
+    }
+
+    private void Distance()
+    {
+        minDis = 100;
+
+        for (int i = 0; i < spots.Length; i++)
+        {
+            dis = Vector2.Distance(WorldToLocal(mousePosition), ConvertUIToWorldCoordinates(mousePosition, spots[i]));
+
+            if (dis < minDis)
+                minDis = dis;
         }
     }
 
@@ -39,5 +95,14 @@ public class DrawPatten : MonoBehaviour
         Vector2 camRelative = cam.transform.InverseTransformPoint(mousePos);
 
         return camRelative;
+    }
+
+    private Vector3 ConvertUIToWorldCoordinates(Vector2 screenPos, RectTransform rect)
+    {
+        // 스크린 좌표를 UI 요소의 로컬 좌표로 변환
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, screenPos, cam, out Vector2 localPoint);
+
+        // UI 요소의 로컬 좌표를 월드 좌표로 변환
+        return rect.TransformPoint(localPoint);
     }
 }
