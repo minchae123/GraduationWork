7d4c795fc820fe0ad3c4113113ddfb4f60b5b141
Inheoseongbin <seongbin529@gmail.com>
2024-03-10 23:59:08 +0900

    Clamp Fix plz

--- WouldYouClean/Assets/01_Scripts/SpaceObject/SpaceBackground.cs
+++ WouldYouClean/Assets/01_Scripts/SpaceObject/SpaceBackground.cs
@@ -13,22 +13,24 @@ public class SpaceBackground : MonoBehaviour
         _sr = GetComponent<SpriteRenderer>();
     }
 
-    private void Start()
-    {
-        StartCoroutine(Twinkle());
-    }
-
-    //private void Update()
+    //private void Start()
     //{
-    //    _sr.material.SetFloat("_Threshold", _brightness);
+    //    StartCoroutine(Twinkle());
     //}
 
-    private IEnumerator Twinkle()
+    private void Update()
     {
-        _sr.material.SetFloat("_Threshold", .965f);
-        yield return new WaitForSeconds(1);
-        _sr.material.SetFloat("_Threshold", .975f);
-        yield return new WaitForSeconds(1);
-        StartCoroutine(Twinkle());
+        _brightness = Mathf.Clamp(_brightness, 0.965f, 0.975f);
+        _brightness = Mathf.PingPong(Time.time, 1f);
+        _sr.material.SetFloat("_Threshold", _brightness);
     }
+
+    //private IEnumerator Twinkle()
+    //{
+    //    _sr.material.SetFloat("_Threshold", .965f);
+    //    yield return new WaitForSeconds(1);
+    //    _sr.material.SetFloat("_Threshold", .975f);
+    //    yield return new WaitForSeconds(1);
+    //    StartCoroutine(Twinkle());
+    //}
 }
