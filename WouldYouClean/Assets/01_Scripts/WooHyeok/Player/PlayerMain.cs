61e1206d3b9a388b52baedd3ace9fc0cf152d772
Woo Hyeok <pwh060720@gmail.com>
2024-03-18 19:58:30 +0900

    애니메이션 수정 완

--- WouldYouClean/Assets/01_Scripts/WooHyeok/Player/PlayerMain.cs
+++ WouldYouClean/Assets/01_Scripts/WooHyeok/Player/PlayerMain.cs
@@ -11,7 +11,6 @@ public  class PlayerMain : MonoBehaviour
     public static bool _isFKeyDown = false;
     public static bool _isQKeyDown = false;
     public static bool _isPlain = false;
-    public static bool _isAnim = false;
 
     private void Start()
     {
