371ed0566a90413a2673ba6a0a7831f66fa411f2
dksgudwn <dksgudwn620@gmail.com>
2024-03-19 11:01:16 +0900

    .

--- WouldYouClean/Assets/01_Scripts/AIUFO.cs
+++ WouldYouClean/Assets/01_Scripts/AIUFO.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class AIUFO : SpaceObject
 {
     public float speed;
+    Vector2 direction;
 
     private void Awake()
     {
@@ -15,10 +16,21 @@ public class AIUFO : SpaceObject
         Vector2 movement = transform.up * speed;
         transform.position += new Vector3(movement.x, movement.y) * Time.deltaTime;
 
-        Vector2 direction = (Vector2)GameManager.Instance.SpaceShipTrm.position - _rb.position;
+        direction = (Vector2)GameManager.Instance.SpaceShipTrm.position - _rb.position;
         float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
 
         // Tweening을 사용하여 부드럽게 회전하도록 합니다.
         DOVirtual.Float(_rb.rotation, targetAngle, 0.5f, angle => _rb.rotation = angle);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Spaceship"))
+        {
+            print("펑터졌다.이펙트랑이것저것밀려나는것도추가하기");
+            SetDir(Vector2.up * 100);
+            print(new Vector2(direction.x + 1, direction.y + 1) * 100);
+            //gameObject.SetActive();
+        }
     }
 }
