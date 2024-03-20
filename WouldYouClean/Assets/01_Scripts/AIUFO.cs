a284cd6c16f3c6a9c7e28f9376a3ef0926be0574
dksgudwn <dksgudwn620@gmail.com>
2024-03-20 22:05:42 +0900

    완성했습니다

--- WouldYouClean/Assets/01_Scripts/AIUFO.cs
+++ WouldYouClean/Assets/01_Scripts/AIUFO.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class AIUFO : SpaceObject
@@ -6,6 +7,8 @@ public class AIUFO : SpaceObject
     public float speed;
     Vector2 direction;
     SpaceShip spaceShip;
+    private float rotateSpeed = 10;
+    private bool isChoosingState = false; // 상태를 선택 중인지 여부를 나타내는 플래그
 
     private void Awake()
     {
@@ -16,20 +19,50 @@ public class AIUFO : SpaceObject
     private void Update()
     {
         Vector2 movement = transform.up * speed;
-        //transform.position += new Vector3(movement.x, movement.y) * Time.deltaTime;
+        SetDir(-spaceShip.transform.up * spaceShip.curSpeed, movement);
 
         if (Vector3.Distance(_rb.position, GameManager.Instance.SpaceShipTrm.position) < 30)//30보다 가까우면 달려들고
         {
-            SetDir(-spaceShip.transform.up * spaceShip.curSpeed, movement);
+            direction = (Vector2)GameManager.Instance.SpaceShipTrm.position - _rb.position;
+            float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            DOVirtual.Float(_rb.rotation, targetAngle, 0.5f, angle => _rb.rotation = angle);
         }
-        else //30보다 멀면 그냥 떠다니기
+        else if (Vector3.Distance(_rb.position, GameManager.Instance.SpaceShipTrm.position) < 200)
         {
-            SetDir(-spaceShip.transform.up * spaceShip.curSpeed);
+            if (!isChoosingState)
+            {
+                StartCoroutine(ChooseState());
+            }
+            transform.Rotate(0, 0, Time.deltaTime * rotateSpeed);
         }
-        direction = (Vector2)GameManager.Instance.SpaceShipTrm.position - _rb.position;
-        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        else
+        {
+            direction = (Vector2)GameManager.Instance.SpaceShipTrm.position - _rb.position;
+            float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            DOVirtual.Float(_rb.rotation, targetAngle, 0.5f, angle => _rb.rotation = angle);
+        }
+    }
 
+    private IEnumerator ChooseState()
+    {
+        isChoosingState = true;
 
-        DOVirtual.Float(_rb.rotation, targetAngle, 0.5f, angle => _rb.rotation = angle);
+        int r = Random.Range(0, 3);
+        int r2 = Random.Range(10, 30);
+        if (r == 0)
+        {
+            rotateSpeed = 0;
+        }
+        else if (r == 1)
+        {
+            rotateSpeed = -r2;
+        }
+        else if (r == 2)
+        {
+            rotateSpeed = r2;
+        }
+        yield return new WaitForSeconds(2f);
+
+        isChoosingState = false;
     }
 }
