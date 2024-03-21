c07e432fb19c6f2df2cc040d5e24dd2c35b83c92
dksgudwn <dksgudwn620@gmail.com>
2024-03-21 10:57:16 +0900

    우주쓰레기만들기

--- WouldYouClean/Assets/01_Scripts/SpaceObject/SpaceGarbageSpawner.cs
+++ WouldYouClean/Assets/01_Scripts/SpaceObject/SpaceGarbageSpawner.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 public class SpaceGarbageSpawner : MonoBehaviour
 {
     public List<ObjectType> TrashTypeList = new List<ObjectType>();
-    public List<GameObject> SpaceTrashList = new List<GameObject>();
+    public List<SpaceObject> SpaceTrashList = new ();
     public GameObject _garbagePrefab;
 
     private void Start()
     {
         for (int i = 0; i < 100; i++)
         {
-            print("dd");
             GameObject newObj = Instantiate(_garbagePrefab.gameObject, transform);
             newObj.GetComponent<DivideObj>().type = TrashTypeList[Random.Range(0, TrashTypeList.Count)];
-            SpaceTrashList.Add(newObj);
+            SpaceTrashList.Add(newObj.GetComponent<SpaceObject>());
             newObj.transform.parent = transform;
-            newObj.transform.position = Random.insideUnitCircle * 200;
+            newObj.transform.position = (Vector3)Random.insideUnitCircle * 200 + transform.position;
         }
     }
 }
