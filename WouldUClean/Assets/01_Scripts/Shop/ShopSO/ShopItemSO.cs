847a899a4f03a04eb6a38c562c8254247763ea1e
ye0jin <fruitfighterlim@gmail.com>
2024-04-09 09:55:13 +0900

    스크립트 정리

---
+++ WouldUClean/Assets/01_Scripts/Shop/ShopSO/ShopItemSO.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PurchaseItem
+{       
+    NONE = 0,
+    Debt = 1,
+    Inventory = 2,
+    O2Tank = 3,
+    HP = 4,  
+    UFO = 5,
+    Item = 6,  
+}
+[CreateAssetMenu(menuName ="SO/Shop/ShopItem")]
+public class ShopItemSO : ScriptableObject
+{
+    [Header("기본적인 아이템 정보")]
+    public PurchaseItem Item;
+
+    // 이름
+    public string itemName;
+    //설명
+    [TextArea] public string itemInfo;
+    // 아이콘
+    public Sprite itemIcon;
+    //가격
+    public int itemPrice;
+
+    public void IsOnSale(int discountPercentage) // 세일 같은거? 나중에 쓰
+    {
+        itemPrice = (int)(itemPrice + itemPrice * (discountPercentage / 100));
+    }
+}
