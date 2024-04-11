239a9d7b2c10d52df568c9c28c779f5388478337
dksgudwn <dksgudwn620@gmail.com>
2024-04-11 12:44:46 +0900

    nononono

---
+++ MeetBall/Assets/01_Scripts/ONZ/move1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move1 : MonoBehaviour
+{
+    private void Start()
+    {
+        Vector3Int vvv = new Vector3Int(1, 2, 3);
+    }
+}
