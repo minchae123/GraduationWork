8df31c25697c0bcd22d35900e324e7f43b5785ba
dksgudwn <dksgudwn620@gmail.com>
2024-04-11 14:14:07 +0900

    .

--- MeetBall/Assets/01_Scripts/ONZ/move1.cs
+++ MeetBall/Assets/01_Scripts/ONZ/move1.cs
@@ -4,8 +4,32 @@ using UnityEngine;
 
 public class move1 : MonoBehaviour
 {
+    public int MoveCount;
+
     private void Start()
     {
-        Vector3Int vvv = new Vector3Int(1, 2, 3);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3Int(1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3Int(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3Int(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3Int(0, 1, 0);
+        }
+
     }
 }
