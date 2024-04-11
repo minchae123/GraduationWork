3c82ecb7d649373ea452a99c57b89b8004e5201c
dksgudwn <dksgudwn620@gmail.com>
2024-04-11 19:45:50 +0900

    .

--- MeetBall/Assets/01_Scripts/ONZ/Move.cs
+++ MeetBall/Assets/01_Scripts/ONZ/Move.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class VecMap
+{
+    public int X;
+    public int Y;
+}
+
 public class Move : MonoBehaviour
 {
     //[SerializeField] private List<VecZ> vec;
+
+    [SerializeField] private List<VecMap> map;
+
+    KeyValuePair<int, VecMap> current;
 }
