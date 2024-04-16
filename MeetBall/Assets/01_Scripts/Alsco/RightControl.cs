83e4eab1609c6f9c56281153dd0ad5417c41d454
minchae123 <08minchae17@gmail.com>
2024-04-16 09:29:49 +0900

    움직임 카운트 추가

--- MeetBall/Assets/01_Scripts/Alsco/RightControl.cs
+++ MeetBall/Assets/01_Scripts/Alsco/RightControl.cs
@@ -6,31 +6,56 @@ public class RightControl : MonoBehaviour
 {
 	private WASD WASD;
 
+	[SerializeField] private int moveCount;
+	private int curCount;
+	private Vector3 startPos;
+
+	private void Start()
+	{
+		startPos = transform.position;
+		curCount = moveCount;
+	}
+
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.UpArrow))
+		if(curCount > 0)
 		{
-			transform.position += WASD.w;
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				transform.position += WASD.w;
+				curCount--;
+			}
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				transform.position += WASD.s;
+				curCount--;
+			}
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				transform.position += WASD.d;
+				curCount--;
+			}
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				transform.position += WASD.a;
+				curCount--;
+			}
+			if (Input.GetKeyDown(KeyCode.Return))
+			{
+				transform.position += Vector3.up;
+				curCount--;
+			}
+			if (Input.GetKeyDown(KeyCode.RightShift))
+			{
+				transform.position += Vector3.down;
+				curCount--;
+			}
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
+
+		if (Input.GetKeyDown(KeyCode.R))
 		{
-			transform.position += WASD.s;
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			transform.position += WASD.d;
-		}
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			transform.position += WASD.a;
-		}
-		if (Input.GetKeyDown(KeyCode.Return))
-		{
-			transform.position += Vector3.up;
-		}
-		if (Input.GetKeyDown(KeyCode.RightShift))
-		{
-			transform.position += Vector3.down;
+			transform.position = startPos;
+			curCount = moveCount;
 		}
 	}
 
