c4c9f7184d23c45a8aa111baf1d88a6281739e69
ye0jin <fruitfighterlim@gmail.com>
2024-04-18 19:17:54 +0900

    버튼색이랑 스테이지 고민중

--- MeetBall/Assets/01_Scripts/button/ToggleButton.cs
+++ MeetBall/Assets/01_Scripts/button/ToggleButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ToggleButton : MonoBehaviour
 {
@@ -14,14 +15,29 @@ public class ToggleButton : MonoBehaviour
         {
             if(isClick) // 누른 적이 있는데 다시 눌렀을 경우
             {
-                connectDoor.Close();
-                isClick = false;
+                DoorClose();
             }
             else // 반대
             {
-                connectDoor.Open();
-                isClick = true;
+                DoorOpen();
             }
         }
+    }
+
+    public void DoorClose()
+    {
+        connectDoor.Close();
+        isClick = false;
+
+        transform.DOScaleY(0.25f, 0.5f);
+        transform.localPosition = new Vector3(transform.localPosition.x, -0.3f, transform.localPosition.z);
+    }
+    public void DoorOpen()
+    {
+        connectDoor.Open();
+        isClick = true;
+
+        transform.DOScaleY(0.1f, 0.5f);
+        transform.localPosition = new Vector3(transform.localPosition.x, -0.45f, transform.localPosition.z);
     }
 }
