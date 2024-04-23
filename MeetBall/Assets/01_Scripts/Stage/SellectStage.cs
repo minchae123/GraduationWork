1a4ae981658d0804bf1e84570fb136e6e7565561
ye0jin <fruitfighterlim@gmail.com>
2024-04-23 19:26:32 +0900

    스테이지선택ui햇는데 위치조정을해야함

--- MeetBall/Assets/01_Scripts/Stage/SellectStage.cs
+++ MeetBall/Assets/01_Scripts/Stage/SellectStage.cs
@@ -1,15 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SellectStage : MonoBehaviour
 {
+    [SerializeField] private StageListSO stageSO;
+    [SerializeField] private StageUI stagePrefab;
+
     [SerializeField] private Transform stageParent;
     public StageUI[] stages;
+    public float[] xPos;
+    private int selectedStage = 0;
 
     private void Start()
     {
-        stages = stageParent.GetComponentsInChildren<StageUI>();
-        stages[0].Selected();
+        xPos = new float[stageSO.Stages.Count];
+        stages = new StageUI[stageSO.Stages.Count];
+
+        for (int i = 0; i < stageSO.Stages.Count; ++i)
+        {
+            StageUI newStage = Instantiate(stagePrefab, stageParent);
+            newStage.SetUI(stageSO.Stages[i], i + 1);
+
+            stages[i] = newStage;
+        }
+
+        for(int i = 0; i < stages.Length; ++i)
+        {
+            xPos[i] = 900 - 290 * i;
+        }
+
+        stages[selectedStage].Selected();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            UpdateStageUI(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            UpdateStageUI(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print(stages[selectedStage].stage);
+        }
+    }
+
+    private void UpdateStageUI(int value)
+    {
+        stages[selectedStage].UnSelected();
+
+        selectedStage += value;
+        selectedStage = Mathf.Clamp(selectedStage, 0, stages.Length - 1);
+
+        stageParent.DOLocalMoveX(xPos[selectedStage], 0.5f);
+        stages[selectedStage].Selected();
     }
 }
--- MeetBall/Assets/01_Scripts/Stage/StageUI.cs
+++ MeetBall/Assets/01_Scripts/Stage/StageUI.cs
@@ -8,7 +8,9 @@ using System;
 
 public class StageUI : MonoBehaviour
 {
+    public StageSO stage;
     public bool IsSelected = false;
+    [SerializeField]private TextMeshProUGUI stageNumText;
 
     private void Start()
     {
@@ -18,11 +20,19 @@ public class StageUI : MonoBehaviour
     public void Selected()
     {
         IsSelected = true;
-        transform.DOScale(2f, 0.6f);
+        transform.DOScale(1.3f, 0.6f);
     }
     public void UnSelected()
     {
         IsSelected = false;
         transform.DOScale(1f, 0.6f);
+    }
+
+    internal void SetUI(StageSO stage, int stageNum)
+    {
+        if (stageNum < 10) stageNumText.text = $"0{stageNum}";
+        else stageNumText.text = $"{stageNum}";
+
+        this.stage = stage;
     }
 }
