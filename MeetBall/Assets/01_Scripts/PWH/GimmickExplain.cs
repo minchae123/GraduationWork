1d25dfd55888da76c242bee9a964c55e286776fd
Woo Hyeok <pwh060720@gmail.com>
2024-05-21 19:47:09 +0900

    기믹 튜토리얼 까쥐~ 하는 중

--- MeetBall/Assets/01_Scripts/PWH/GimmickExplain.cs
+++ MeetBall/Assets/01_Scripts/PWH/GimmickExplain.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GimmickExplain : MonoBehaviour
 {
+    [SerializeField] private string[] explain;
+    [SerializeField] private int objLayer;
+    [SerializeField] private TextMeshProUGUI explainTxt;
+
     private void Update()
     {
         CickObj();
@@ -18,7 +24,26 @@ public class GimmickExplain : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            Debug.Log(hit.transform.name);
+            LayerMask layer = hit.transform.gameObject.layer;
+
+            for(int i = objLayer; i < objLayer + 5; i++)//나중에 기믹 수에 맞게 포문 돌아가게 바꾸기
+            {
+                if(layer.value == i)
+                {
+                    explainTxt.text = explain[i - objLayer];
+                    break;
+                }
+            }
+
+            Vector3 hitPos = Camera.main.WorldToScreenPoint(hit.transform.position);
+            transform.position = hitPos;
+;
+            if (TryGetComponent(out TutorialPanel panel))
+                panel.ShowTutorial();
+        }
+        else
+        {
+
         }
     }
 }
