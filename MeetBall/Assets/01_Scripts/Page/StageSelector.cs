3b750c626c107a4f6432df2d498736bd7d7c3e2b
dksgudwn <dksgudwn620@gmail.com>
2024-07-03 19:57:41 +0900

    씬 바구기

---
+++ MeetBall/Assets/01_Scripts/Page/StageSelector.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using echo17.EndlessBook;
+using echo17.EndlessBook.Demo02;
+using System;
+
+
+public class StageSelector : PageView
+{
+    [Serializable]
+    public struct stageInfo
+    {
+        public string gameObjectName;
+        public int stageNumber;
+    }
+
+    public stageInfo[] stageInfos;
+
+    protected override bool HandleHit(RaycastHit hit, BookActionDelegate action = null)
+    {
+        if (action == null) return false;
+        foreach (var stageInfo in stageInfos)
+        {
+            if (stageInfo.gameObjectName == hit.collider.gameObject.name)
+            {
+                print(stageInfo.gameObjectName);
+                print(stageInfo.stageNumber);
+                //GameManager.Instance.game.SetActive(true);
+                //StageManager.Instance.SetStageNumber(stageInfo.stageNumber);
+                //GameManager.Instance.book.SetActive(false);
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+}
